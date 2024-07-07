c6c49386716490df1b0d51dfe9c6af6eb98d9af5
DenisHrtn <denispython16@gmail.com>
2024-07-07 16:03:15 +0300

    update schedules

--- schedule_core/schedule/serializers/schedule_serializer.py
+++ schedule_core/schedule/serializers/schedule_serializer.py
@@ -11,10 +11,35 @@ class ScheduleSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         if Schedule.objects.filter(user=self.context['request'].user, title=validated_data['title']).exists():
-            raise serializers.ValidationError({'title': 'This title is already in use.'})
+            raise serializers.ValidationError({'title': "This title is already in use."})
 
         if validated_data['due_date'] and validated_data['due_date'] < datetime.date.today():
-            raise serializers.ValidationError({'due_date': 'Date cannot be in the future.'})
+            raise serializers.ValidationError({'due_date': "Date cannot be in the future."})
 
-        schedule = Schedule.objects.create(user=self.context['request'].user,**validated_data)
+        schedule = Schedule.objects.create(user=self.context['request'].user, **validated_data)
+
         return schedule
+
+    def update(self, instance, validated_data):
+        title = validated_data.get('title', instance.title)
+        due_date = validated_data.get('due_date', instance.due_date)
+
+        if Schedule.objects.filter(user=self.context['request'].user, title=title).exclude(pk=instance.pk).exists():
+            raise serializers.ValidationError({'title': "This title is already in use."})
+
+        if due_date and due_date < datetime.date.today():
+            raise serializers.ValidationError({'due_date': "Date cannot be in the future."})
+
+        if instance.completed:
+            raise serializers.ValidationError({'completed': "Task is already completed."})
+
+        instance.title = title
+        instance.due_date = due_date
+        instance.description = validated_data.get('description', instance.description)
+        instance.category = validated_data.get('category', instance.category)
+        instance.mark = validated_data.get('mark', instance.mark)
+        instance.completed = validated_data.get('completed', instance.completed)
+        instance.updated_at = datetime.datetime.now()
+        instance.save()
+
+        return instance
--- schedule_core/schedule/views/schedule.py
+++ schedule_core/schedule/views/schedule.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework import parsers
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
 
 from schedule_core.swagger_service.apply_swagger_auto_schema import apply_swagger_auto_schema
 from schedule.serializers import ScheduleSerializer
@@ -12,7 +14,15 @@ class ScheduleViewSet(viewsets.ModelViewSet):
     parser_classes = [parsers.MultiPartParser, ]
 
     def get_queryset(self):
-        return self.request.user.schedules_users.select_related('category', 'mark')
+        return self.request.user.schedules_users.select_related('category', 'mark').defer('user')
+
+    # @swagger_auto_schema(auto_schema=None)
+    # def update(self, request, *args, **kwargs):
+    #     return Response("Method Not Allowed", status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    # @swagger_auto_schema(request_body=ScheduleSerializer)
+    # def partial_update(self, request, *args, **kwargs):
+    #     return super().partial_update(request, *args, **kwargs)
 
 
 ScheduleViewSet = apply_swagger_auto_schema(
