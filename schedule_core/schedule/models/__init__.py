07db6a32bdfb822675efa67cdb2e49125f656a9e
DenisHrtn <denispython16@gmail.com>
2024-09-06 14:56:06 +0300

    delete priority model

--- schedule_core/schedule/models/__init__.py
+++ schedule_core/schedule/models/__init__.py
@@ -1,5 +1,4 @@
 from .category import Category # noqa
-from .priority import Priority # noqa
 from .schedule import Schedule # noqa
 from .history import History # noqa
 from .attachment import Attachment # noqa
--- schedule_core/schedule/models/priority.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Priority(models.Model):
-    name = models.CharField(max_length=255)
-    level = models.PositiveSmallIntegerField(default=0)
-
-    def __str__(self):
-        return self.name
